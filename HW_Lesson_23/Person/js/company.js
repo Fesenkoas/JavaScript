45ae7b3a5e17320e84e226ad9d3953665b154ed1
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-06-03 11:25:33 +0300

    Add files via upload

---
+++ HW_Lesson_23/Person/js/company.js
@@ -0,0 +1,53 @@
+
+
+class Company {
+    constructor() {
+        this._employees = [];
+    }
+
+    get employees() {
+        return [...this._employees];
+    }
+    searchEmployee(employee) {
+        const index = this._employees.findIndex(e => e.id === employee.id);
+
+        return index < 0;
+    }
+    
+    addEmployee(employee) {
+        const index = this._employees.findIndex(e => e.id === employee.id);
+        if (index < 0) {
+            this._employees.push(employee);
+        }
+        return index < 0;
+    }
+
+    removeEmployee(id){
+        const index = this._employees.findIndex(e => e.id === id);
+        if(index >= 0){
+            this._employees.splice(index, 1);
+        }
+        return index >= 0;
+    }
+
+    maxAge(){
+        return this._employees.reduce((max, p) => p.age > max ? p.age : max, this.employees[0].age);
+    }
+
+    minAge(){
+        return this._employees.reduce((min, p) => p.age < min ? p.age : min, this.employees[0].age);
+    }
+
+    averageAge(){
+        return this._employees.reduce((accum, p) => accum + p.age, 0) / this.employees.length;
+    }
+
+    averageSalary(){
+        return this._employees.reduce((accum, p) => accum + p.salary, 0) / this.employees.length;
+    }
+
+    totalSalary(){
+        return this._employees.reduce((accum, p) => accum + p.salary, 0);
+    }
+
+}
---
+++ HW_Lesson_23/Person/js/script.js
@@ -0,0 +1,82 @@
+const firm = new Company();
+
+//Event Keyboard
+addEventListener("keydown", (e) => {
+  if (e.key === "Enter") addPersons();
+});
+//Event Mouse
+addEventListener("click", (e) => {
+  if (e.target === addPerson) addPersons();
+  if (e.target === calcStats) printStats();
+});
+//Add Persons
+function addPersons() {
+  const [id, fName, lName, Age, Salary] = [personId.value.trim(), firstName.value.trim(), lastName.value.trim(), age.value.trim(), salary.value.trim()];
+  if (id && fName && lName && Age && Salary) {
+    const person= (new Employee(id, fName, lName, Age, Salary));
+    if (firm.addEmployee(person)){
+    
+    const buttonDel = document.createElement('button');
+          buttonDel.append(document.createTextNode('X'));
+          buttonDel.classList.add('del');
+
+        const tr = document.createElement('tr');// main block
+        
+        tr.appendChild(createInfoElement(`${person.id}`,'td'));
+        tr.appendChild(createInfoElement(`${person.firstName}`,'td'));
+        tr.appendChild(createInfoElement(`${person.lastName}`,'td'));
+        tr.appendChild(createInfoElement(`${person.age}`,'td'));
+        tr.appendChild(createInfoElement(`${person.salary}`,'td'));
+        tr.appendChild(buttonDel);
+        personsList.append(tr);
+        
+          buttonDel.onclick = e => {
+            const index = firm.employees.findIndex(item => item.id === person.id);
+            firm.removeEmployee(firm.employees[index].id);
+            e.target.parentElement.remove();
+            delStats();
+            printStats();
+        } 
+        
+    }
+    else {
+      alert(`Person with id = ${id} exists`);
+    }
+  } 
+  else alert("Enter all date");
+  printStats();
+  personId.value = firstName.value = lastName.value = age.value = salary.value = '';
+}
+
+//Print Statistic
+function printStats() {
+  delStats();
+    
+  const tr = document.createElement('tr');// main block
+    //MAX
+    tr.appendChild(createInfoElement(`${firm.maxAge()}`,'td'));
+    //MIN
+    tr.appendChild(createInfoElement(`${firm.minAge()}`,'td'));
+    //Average Age
+    tr.appendChild(createInfoElement(`${firm.averageAge()}`,'td'));
+    //Average Salary
+    tr.appendChild(createInfoElement(`${firm.averageSalary()}`,'td'));
+    //Total Salary
+    tr.appendChild(createInfoElement(`${firm.totalSalary()}`,'td'));
+    stats.append(tr);
+}
+
+// Create Elemets HTML
+function createInfoElement(content, tag) {
+  const element = document.createElement(tag);
+  const text = document.createTextNode(content);
+  element.appendChild(text);
+  return element;
+}
+
+
+function delStats() {
+  if (stats.firstElementChild) {
+      stats.removeChild(stats.firstElementChild);
+  }
+}

dff3c7b96fb8a7a1ba3818ff98ef97702a61185a
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-06-16 18:05:01 +0300

    check

--- HW_Lesson_24/async.js
+++ HW_Lesson_24/async.js
@@ -35,3 +35,43 @@
 //         console.log(i);
 //     },1000);
 // }
+
+//Solution 1 -> use let
+// for(let i = 0; i < 10; i++){
+//     setTimeout(function () {
+//         console.log(i);
+//     },1000);
+// }
+
+//Solution 2 -> use wrapper function
+// for (var i = 0; i < 10; i++) {
+//     function task(j) {
+//         setTimeout(function () {
+//             console.log(j);
+//         }, 1000);
+//     }
+//     task(i);
+// }
+
+//Solution 3 -> use anonimous closure
+// for (var i = 0; i < 10; i++) {
+//     (function(j) {
+//         setTimeout(function () {
+//             console.log(j);
+//         }, 1000);
+//     })(i)  
+// }
+
+//Solution 4 -> use setTimeout args
+// for(var i = 0; i < 10; i++){
+//     setTimeout(function (j) {
+//         console.log(j);
+//     },1000, i);
+// }
+
+//Solution 5 -> for fun
+for(var i = 0; i < 10; i++){
+    setTimeout(function () {
+        console.log(i++ % 10);
+    },1000);
+}

46dc48e190e359a455b9ac645df9e0c7007b6653
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-07-13 21:17:50 +0300

    check

--- starwars/src/component/Contact/Contact.jsx
+++
@@ -1,8 +0,0 @@
-
-const Contact = () =>{
-    return(<div> 
-     <div><h1>Contact</h1></div>
-     </div>)
-}
-
-export default Contact;
--- starwars/src/component/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-import s from '../css/style.module.css'
-import Main from './Main';
-
-const Home = () =>{
-    return <Main s={s}/>
-}
-
-export default Home;

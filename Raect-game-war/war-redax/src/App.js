2e8bb41a9bd2fd35d8192b62b78b9e6ab14ec1ae
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-09-21 06:12:10 +0300

    check

--- Raect-game-war/war-redax/src/App.js
+++ Raect-game-war/war-redax/src/App.js
@@ -1,18 +1,16 @@
-
 import Game from "./components/Game";
 import Result from "./components/Result";
 import Start from "./components/Start";
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 const App = () => {
-  
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Start/>} />
-      <Route path="/game" element={<Game />} />
-      <Route path="/result" element={<Result />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </BrowserRouter>
   );
 };
